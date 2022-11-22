708c167c2016fd1c4ea76d859af9762822ec70ca
NgocHungSmartDev <hoang.ngoc.hungit@gmail.com>
2022-11-22 23:01:18 +0700

    Add menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { RoomComponent } from './admin/room/room.component';
+import { UserComponent } from './admin/user/user.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    RoomComponent,
+    UserComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
